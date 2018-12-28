bbbf690f861ccb22fc93a190d851d7517ee0086f
thegouge <thegouge42@gmail.com>
2018-12-28 11:39:22 -0800

    initialize metadata and start building the project

---
+++ src/components/Timer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Timer(props) {
+  return (
+    <div id="timer">
+      <h2 id="timer-label">Session/Break</h2>
+      <div id="time-left" />
+    </div>
+  );
+}
---
+++ src/components/Toolbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Toolbar(props) {
+  return (
+    <div>
+      <div id="session-stuff" />
+      <div id="break-stuff" />
+    </div>
+  );
+}
