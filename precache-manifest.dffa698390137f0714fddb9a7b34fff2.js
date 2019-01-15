7c8944d4ad34ea525667caa2cdb3a4a2f1c92837
thegouge <thegouge42@gmail.com>
2019-01-14 19:06:27 -0800

    Updates

---
+++ precache-manifest.dffa698390137f0714fddb9a7b34fff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71950975267c8e97b577abd6158a7f08",
+    "url": "/fcc-pomodoro-clock/static/media/Rooster-Crow.71950975.wav"
+  },
+  {
+    "revision": "1ad5dc8ced7a11fe8830",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.1ad5dc8c.js"
+  },
+  {
+    "revision": "a55da39c3bbb2f2fba9e",
+    "url": "/fcc-pomodoro-clock/static/js/main.a55da39c.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/fcc-pomodoro-clock/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "a55da39c3bbb2f2fba9e",
+    "url": "/fcc-pomodoro-clock/static/css/main.d6b3e969.chunk.css"
+  },
+  {
+    "revision": "3e678a9b9aa02b921a9cc9b183c4f6d9",
+    "url": "/fcc-pomodoro-clock/index.html"
+  }
+];
