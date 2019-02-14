ba9660579cb9394714c67ca98ae5299118e19c31
thegouge <thegouge42@gmail.com>
2019-02-13 18:29:59 -0800

    updated footer to be uniform across projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,5 +3,10 @@ import React from "react";
 import "../css/footer.css";
 
 export default function Footer() {
-  return <footer>Made by Alex!</footer>;
+  return (
+    <footer>
+      this is a project made for the Free Code Camp "Front End Libraries"
+      Certification by <strong>Alex Gouge-Schajer</strong>
+    </footer>
+  );
 }
